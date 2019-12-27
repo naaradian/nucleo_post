c2bce992db3f0ccb322ffa213d380486b5cb20a7
naaradian <happymail@mail.ru>
2019-12-27 16:12:10 +0300

    start

---
+++ Debug/Src/mbascii.d
@@ -0,0 +1,40 @@
+Src/mbascii.o: ../Src/mbascii.c \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/port.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mb.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbconfig.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/port.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbport.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbproto.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbfunc.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbconfig.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbascii.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbframe.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mb.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbcrc.h \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbport.h
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/port.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mb.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbconfig.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/port.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbport.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbproto.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbfunc.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbconfig.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbascii.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbframe.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mb.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbcrc.h:
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/mbport.h:
---
+++ Debug/Src/mbcrc.d
@@ -0,0 +1,4 @@
+Src/mbcrc.o: ../Src/mbcrc.c \
+ D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/port.h
+
+D:/Users/user/keil/stm32cubemx/three_discav/mb6_post/Drivers/modbus/include/port.h:
